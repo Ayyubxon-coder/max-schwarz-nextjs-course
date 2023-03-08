f5fcff2ae93eacb9529e510300581e9bcf77e7e6
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-03-08 19:10:38 +0500

    layout page added

--- dummy-news/pages/index.js
+++ dummy-news/pages/index.js
@@ -1,3 +1,4 @@
+import Layout from '@/components/layout/Layout';
 import MeetupList from '@/components/meetups/MeetupList';
 const DUMMY_MEETUPS = [
   {
@@ -18,6 +19,10 @@ const DUMMY_MEETUPS = [
   },
 ];
 function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 }
 export default HomePage;
